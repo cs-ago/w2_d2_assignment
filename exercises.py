95f9b3760c27551b867f7f03adc8d978484d7b8c
Agoston Csehi <csehi.agoston.benedek@gmail.com>
2025-01-14 12:30:06 +0100

    Exercise 2 + pdfs + modified gitignore (to upload pdfs)

--- exercises.py
+++ exercises.py
@@ -13,18 +13,29 @@ for measurement in precipitation_data:
 #EXERCISE 1.3: TOTAL MONTHLY PERCIPITATION FOR SEATTLE
 #Crate a list of 12 zeros for the months
 seattle_total_monthly_precipitation = [0]*12
+#EXERCISE 2.1: CALCUATE THE TOTAL YEARLY PERCIPITATION
+seattle_total_yearly_precipitation = 0
 for measurement in seattle_data:
     #Calculate the index of the month by subtracting 1
     month_index = int(measurement['date'].split('-')[1]) - 1
-    #Add the value
+    #Add the value to both the monthly and the yearly total
     seattle_total_monthly_precipitation[month_index] += measurement['value']
-
-#EXERCISE 1.4: Save the results to a json file
+    seattle_total_yearly_precipitation += measurement['value']
+    
+#EXERCISE 2.2: CALCULATE RELATIVE MONTHLY PERCIPITATION
+seattle_relative_monthly_precipitation = [0]*12
+for month_index in range(0,12):
+    #Define the relative monthly precipitation as a proportion of the monthly and yearly total
+    seattle_relative_monthly_precipitation[month_index] = seattle_total_monthly_precipitation[month_index] / seattle_total_yearly_precipitation
+    
+#EXERCISE 1.4 & 2.3: Save the results to a json file
 output_data = {
     'Seattle': {
         'station': 'GHCND:US1WAKG0038',
         'state': 'WA',
-        'total_monthly_precipitation': seattle_total_monthly_precipitation
+        'total_monthly_precipitation': seattle_total_monthly_precipitation,
+        'total_yearly_precipitation': seattle_total_yearly_precipitation,
+        'relative_monthly_precipitation': seattle_relative_monthly_precipitation
     }
 }
 with open('results.json', 'w', encoding='utf-8') as file:
